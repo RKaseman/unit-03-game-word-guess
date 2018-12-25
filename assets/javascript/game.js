b5744bc04fa7f2ee40db9d899d41f3118a2fb098
Rane M Kaseman <raykman@comcast.net>
2018-12-25 16:21:19 -0700

    logic progress 1621

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,32 +15,29 @@ var wordPicked = Math.floor(Math.random() * wordArray.length);
 console.log("wordArray[wordPicked] = " + wordArray[wordPicked]);
 
 function wordInitialize() {
-
     var lettersToGuess = wordArray[wordPicked].split("");
     console.log("lettersToGuess = " + lettersToGuess);
-
     for (var i = 0; i < lettersToGuess.length; i++) {
-        console.log("lettersToGuess[i] = " + lettersToGuess[i]);
         letters.push(lettersToGuess[i]);
-        document.getElementById("word-to-guess").innerHTML = "<span>" + letters + "</span>";
+        document.getElementById("word-to-guess").innerHTML = "<span>" + letters.join(" ") + "</span>";
     }
-
 }
 
 wordInitialize();
 
 console.log("letters = " + letters);
 
-// document.onkeyup = function (event) {
-//     // Determines which key was pressed.
-//     var letterGuess = event.key;
-//     console.log(event.key);
-//     // var result = wordGen().charAt(4);
-//     // console.log(result);
-//     if (letterGuess === wordGen().charAt(2)) {
-//         document.getElementById("newWord").innerHTML = "test";
-//     }
-// }
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+    console.log("userGuess = " + userGuess);
+    var inArray = letters.includes(userGuess);
+    console.log("inArray = " + inArray);
+    // var result = wordGen().charAt(4);
+    // console.log(result);
+    if (inArray === false) {
+        document.getElementById("message").innerHTML = "That is not a letter in the word";
+    }
+}
 
 // // function myFunction() {
 // //     var str = "HELLO WORLD";
@@ -57,20 +54,5 @@ console.log("letters = " + letters);
 // //     hideWord.setAttribute("type", "hidden");
 // //     document.body.appendChild(hideWord);
 // //     document.getElementById("newWord").innerHTML = wordGen();
-// // }
-
-// // function myFunction2() {
-// //     document.getElementById("demo2").innerHTML = "Paragraph changed.";
-// // }
-
-// // function myFunction3() {
-// //     var str = "Hello World!";
-// //     var n = str.length;
-// //     document.getElementById("demo3").innerHTML = n;
-// // }
-
-// // function myFunction4() {
-// //     var x = document.getElementById("myInput").value;
-// //     document.getElementById("demo4").innerHTML = x;
 // // }
 
