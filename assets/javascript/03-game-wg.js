bcc920353df555d179844abc0414455faf9688e7
Rane M Kaseman <raykman@comcast.net>
2019-01-16 11:47:59 -0700

    ...or tweak the javascript 1147

--- assets/javascript/03-game-wg.js
+++ assets/javascript/03-game-wg.js
@@ -14,16 +14,15 @@ var counter = 0;
 var matchIndex;
 
 var wordPicked = Math.floor(Math.random() * wordArray.length);
-console.log("wordArray[wordPicked] = " + wordArray[wordPicked]);
+// console.log("wordArray[wordPicked] = " + wordArray[wordPicked]);
 document.getElementById("game-state").innerHTML = counter;
 
 function wordInitialize() {
     var lettersToGuess = wordArray[wordPicked].split("");
-    console.log("lettersToGuess = " + lettersToGuess);
-    var i;
+    // console.log("lettersToGuess = " + lettersToGuess);
     for (var i = 0; i < lettersToGuess.length; i++) {
         letters.push(lettersToGuess[i]);
-        document.getElementById("word-to-guess").innerHTML = "<span>" + letters.join("</span><hr><span>") + "</span><hr>";
+        document.getElementById("word-to-guess").innerHTML = "<span>" + letters.join("<hr></span><span>") + "<hr></span>";
     }
 }
 
@@ -33,20 +32,19 @@ console.log("letters = " + letters);
 
 document.onkeyup = function (event) {
     var userGuess = event.key;
-    console.log("userGuess = " + userGuess);
+    // console.log("userGuess = " + userGuess);
     var inArray = letters.includes(userGuess);
     if (inArray) {
         matchIndex = letters.indexOf(userGuess);
-        console.log("matchIndex = " + matchIndex);
+        // console.log("matchIndex = " + matchIndex);
         document.getElementsByTagName("span")[matchIndex].style.visibility = "visible";
         document.getElementById("message").innerHTML = "Good guess";
-        // var j;
         for (var j = matchIndex + 1; j < letters.length; j++) {
             matchIndex++;
-            console.log("matchIndex = " + matchIndex);
-            console.log("span .innerHTML = " + document.getElementsByTagName("span")[matchIndex].innerHTML);
-            if (userGuess === document.getElementsByTagName("span")[matchIndex].innerHTML) {
-                console.log("span .innerHTML = " + document.getElementsByTagName("span")[matchIndex].innerHTML);
+            // console.log("matchIndex = " + matchIndex);
+            // console.log("span .innerHTML = " + document.getElementsByTagName("span")[matchIndex].innerHTML);
+            if (userGuess === document.getElementsByTagName("span")[matchIndex].innerHTML.charAt(0)) {
+                // console.log("span .innerHTML.charAt(0) = " + document.getElementsByTagName("span")[matchIndex].innerHTML.charAt(0));
                 document.getElementsByTagName("span")[matchIndex].style.visibility = "visible";
             }
         }
