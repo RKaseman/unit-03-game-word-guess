854357d9d13c492ec0e1007b2302882478200cbb
Rane M Kaseman <raykman@comcast.net>
2019-01-16 18:42:59 -0700

    del console logs 1842

--- assets/javascript/03-game-wg.js
+++ assets/javascript/03-game-wg.js
@@ -20,11 +20,10 @@ function wordInitialize() {
     letters = [];
     guessed = [];
     counter = 0;
-    document.getElementById("game-state").innerHTML = counter;
     var wordPicked = Math.floor(Math.random() * wordArray.length);
-    console.log("wordArray[wordPicked] = " + wordArray[wordPicked]);
-    document.getElementById("message").innerHTML = "Pick your letter";
     var lettersToGuess = wordArray[wordPicked].split("");
+    document.getElementById("game-state").innerHTML = counter;
+    document.getElementById("message").innerHTML = "Pick your letter";
     for (var i = 0; i < lettersToGuess.length; i++) {
         letters.push(lettersToGuess[i]);
         document.getElementById("word-to-guess").innerHTML = "<span>" + letters.join("<hr></span><span>") + "<hr></span>";
@@ -35,21 +34,16 @@ function wordInitialize() {
             var userGuess = event.key;
             var inGuessedArray = guessed.includes(userGuess);
             var inLettersArray = letters.includes(userGuess);
-            // console.log("cancelable? = " + event.cancelable + " " + event.keyCode);
             if (event.keyCode === 116) {
                 event.preventDefault();
                 document.getElementById("message").innerHTML = "Use the browser's refresh button";
-                console.log("F5 pressed");
                 return;
             }
-            console.log("userGuess = " + userGuess);
-            console.log("inGuessedArray = " + inGuessedArray);
             if (inGuessedArray) {
                 document.getElementById("message").innerHTML = "That letter was already guessed";
                 return;
             }
             guessed.push(userGuess);
-            console.log("guessed = " + guessed);
             if (inLettersArray) {
                 matchIndex = letters.indexOf(userGuess);
                 document.getElementsByTagName("span")[matchIndex].style.visibility = "visible";
@@ -71,14 +65,10 @@ function wordInitialize() {
             document.onkeydown = function (event) {
                 if (event.keyCode != 32) {
                     document.getElementById("message").innerHTML = "Press Space to continue";
-                    console.log("event.keyCode#1 =", event.keyCode != 32);
                     return;
                 }
                 if (event.keyCode === 32) {
-                    console.log("hit");
-                    console.log("event.keyCode#2 =", event.keyCode != 32);
                     wordInitialize();
-                    // return;
                 }
             }
         }
