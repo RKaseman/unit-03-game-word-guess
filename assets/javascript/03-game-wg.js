77a2a0ceaa6e6b66513d50f888eeafcffe6d5f6a
Rane M Kaseman <raykman@comcast.net>
2019-01-18 20:22:21 -0700

    maybe done with game logic 2022

--- assets/javascript/03-game-wg.js
+++ assets/javascript/03-game-wg.js
@@ -10,27 +10,23 @@ var wordArray = [
     "eigth"
 ];
 
-var letters = [];
-var guessed = [];
-var counter = 0;
 var matchIndex;
-var messageP = document.getElementById("message");
-var visibleCount;
+var messageSays = document.getElementById("message");
 
-wordInitialize();
+gameInitialize();
 
-function wordInitialize() {
+function gameInitialize() {
     letters = [];
     guessed = [];
     counter = 0;
     var wordPicked = Math.floor(Math.random() * wordArray.length);
     var lettersToGuess = wordArray[wordPicked].split("");
     document.getElementById("game-state").innerHTML = counter;
-    messageP.innerHTML = "Pick your letter";
+    messageSays.innerHTML = "pick your letter";
     for (var i = 0; i < lettersToGuess.length; i++) {
         letters.push(lettersToGuess[i]);
         document.getElementById("word-to-guess").innerHTML = "<span>" + letters.join("<hr></span><span>") + "<hr></span>";
-    }
+    };
 
     document.onkeydown = function (event) {
         if (counter < 7) {
@@ -39,48 +35,64 @@ function wordInitialize() {
             var inGuessedArray = guessed.includes(userGuess);
             if (event.keyCode === 116) {
                 event.preventDefault();
-                messageP.innerHTML = "Use the browser's refresh button";
+                messageSays.innerHTML = "use the browser's refresh button";
                 return;
-            }
+            };
             if (inGuessedArray) {
-                messageP.innerHTML = "That letter was already guessed";
+                messageSays.innerHTML = "that letter was already guessed";
                 return;
-            }
+            };
             guessed.push(userGuess);
             if (inLettersArray) {
                 matchIndex = letters.indexOf(userGuess);
                 document.getElementsByTagName("span")[matchIndex].style.visibility = "visible";
-                messageP.innerHTML = "Good guess";
+                messageSays.innerHTML = "good guess";
                 for (var j = matchIndex + 1; j < letters.length; j++) {
                     matchIndex++;
                     if (userGuess === document.getElementsByTagName("span")[matchIndex].innerHTML.charAt(0)) {
                         document.getElementsByTagName("span")[matchIndex].style.visibility = "visible";
-                    }
-                }
+                    };
+                };
             } else {
                 counter++;
                 document.getElementById("game-state").innerHTML = counter;
-                messageP.innerHTML = "That is not a letter in the word";
-            }
-        }
+                messageSays.innerHTML = "that is not a letter in the word";
+            };
+        };
         if (counter == 7) {
-            messageP.innerHTML = "Press Space to continue";
+            messageSays.innerHTML = "you lose - press space to continue";
+            gameOver();
+        };
+
+        // letter visibility check
+        testVisibility();
+
+        function testVisibility() {
+            var isVisible = [];
+            for (var k = 0; k < letters.length; k++) {
+                var findVisible = document.getElementsByTagName("span")[k].style.visibility;
+                if (findVisible === "visible") {
+                    isVisible.push(k);
+                    if (isVisible.length == letters.length) {
+                        messageSays.innerHTML = "you win - press space to continue"
+                        gameOver();
+                    };
+                };
+            };
+        };
+
+        function gameOver() {
             document.onkeydown = function (event) {
                 if (event.keyCode != 32) {
-                    messageP.innerHTML = "Press Space to continue";
+                    messageSays.innerHTML = "press space to continue";
                     return;
-                }
+                };
                 if (event.keyCode === 32) {
-                    wordInitialize();
-                }
-            }
-        }
-        // visibility
-        var k;
-        for (var k = 0; k < letters.length; k++) {
-            console.log("visibility = " + document.getElementsByTagName("span")[k].style.visibility);
-            console.log("k = " + k);
-        }
-    }
+                    gameInitialize();
+                };
+            };
+        };
+
+    };
 };
 
