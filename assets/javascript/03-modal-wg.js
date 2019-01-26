110fa5bc6b18087f0c4a2b0d3c05e1373d71f4d9
Rane M Kaseman <raykman@comcast.net>
2019-01-25 20:59:39 -0700

    2059

--- assets/javascript/03-modal-wg.js
+++ assets/javascript/03-modal-wg.js
@@ -1,5 +1,10 @@
 
 $(document).ready(function () {
+
     console.log("modal connected");
+
+    document.getElementById("modal-trig").onclick = function() {
+        document.getElementById("modal-about").style.display = "block";
+    };
 });
 
